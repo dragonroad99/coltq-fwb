PixelExperience/frameworks_base

FaceService: Nuke useless call to getAuthenticatorId
That value is static, so the call isn't needed. That fixes also a logspam:

2021-03-06 21:11:09.553 1767-1767/? W/Binder: Outgoing transactions from this process must be FLAG_ONEWAY
    java.lang.Throwable
        at android.os.BinderProxy.transact(BinderProxy.java:511)
        at com.android.internal.util.custom.faceunlock.IFaceService$Stub$Proxy.getAuthenticatorId(IFaceService.java:335)
        at com.android.server.biometrics.face.CustomFaceService.getAuthenticatorId(CustomFaceService.java:275)
        at com.android.server.biometrics.face.FaceService.updateActiveGroup(FaceService.java:1190)
        at com.android.server.biometrics.BiometricServiceBase.loadAuthenticatorIds(BiometricServiceBase.java:1183)
        at com.android.server.biometrics.face.CustomFaceService$FaceServiceConnection.lambda$onServiceConnected$0$CustomFaceService$FaceServiceConnection(CustomFaceService.java:427)
        at com.android.server.biometrics.face.-$$Lambda$CustomFaceService$FaceServiceConnection$xfTT3adGf0joHpCrnXRrbjd4jD8.run(Unknown Source:2)
        at android.os.Handler.handleCallback(Handler.java:938)
        at android.os.Handler.dispatchMessage(Handler.java:99)
        at android.os.Looper.loop(Looper.java:223)
        at com.android.server.SystemServer.run(SystemServer.java:631)
        at com.android.server.SystemServer.main(SystemServer.java:417)
        at java.lang.reflect.Method.invoke(Native Method)
        at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:592)
        at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:925)

Change-Id: I98d5f4bad9af7faaea8d5333362d080f41a16c85

services/core/java/com/android/server/biometrics/face/FaceService.java
@@ -1185,11 +1185,7 @@ public class FaceService extends BiometricServiceBase {
             mCurrentUserId = userId;
             mCustomFaceService.setCurrentUserId(mCurrentUserId);
             if (mCustomFaceService.getService(mCurrentUserId) != null) {
-                long authId = 0;
-                if (hasEnrolledBiometrics(mCurrentUserId)) {
-                    authId = (long) mCustomFaceService.getAuthenticatorId();
-                }
-                mAuthenticatorIds.put(userId, authId);
+                mAuthenticatorIds.put(userId, (long) 1);
             } else {
                 mCustomFaceService.callForBind(userId);
                 Slog.w(TAG, "updateActiveGroup(): Face service not started!");

dragonroad99/coltq-fwb

Signed-off-by: Ray <vincentrayananda@gmail.com>

services/core/java/com/android/server/biometrics/face/FaceService.java
@@ -1531,11 +1531,7 @@ public class FaceService extends BiometricServiceBase {
                 try {
                     Map map = mAuthenticatorIds;
                     Integer valueOf = Integer.valueOf(mCurrentUserId);
-                    long authId = 0;
-                    if (hasEnrolledBiometrics(mCurrentUserId)) {
-                        authId = (long) service.getAuthenticatorId();
-                    }
-                    map.put(valueOf, Long.valueOf(authId));
+                    map.put(valueOf, (long) 1);
                 } catch (Exception e) {
                     Slog.e(TAG, "getAuthenticatorId failed", e);
                 }